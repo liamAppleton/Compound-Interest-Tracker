641d1667380d808fcb759f3fa251e786ff76c29b
Liam Appleton <liamappleton97@outlook.com>
2025-04-30 13:42:12 +0100

    creating user input questions

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter initial investment amount: £");
+            string initialAmount = Console.ReadLine();
 
+            Console.Write("Enter annual interest rate (in %): ");
+            string annualInterestRate = Console.ReadLine();
+
+            Console.Write("Enter number of years: ");
+            string years = Console.ReadLine();
+
+            Console.Write("Enter compounding frequency (Annually, Semiannually, Quarterly, Monthly, Daily): ");
+            string frequency = Console.ReadLine();
         }
     }
 }
